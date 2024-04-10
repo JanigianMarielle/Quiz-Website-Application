b46fd65fdc9e24f89829576fe93d7eb4fe19eb08
Marielle Janigian <mariellejanigian@Marielles-MacBook-Air.local>
2024-04-10 11:50:57 -0700

    update

--- myscript.js
+++ myscript.js
@@ -1,5 +1,87 @@
 $(document).ready(function() {
-$("#flip").click(function() {
-  $("#panel").click(slideDown(slow);
-}):
-});
+
+  $("#hide").click(function(){
+  $("p").hide();
+ });
+ 
+$("#show1").click(function() {
+$("#panel1").show()
+  });
+  
+$("#hide1").click(function() {
+$("#panel1").hide()
+  });
+  
+  $("#show2").click(function() {
+$("#panel2").show()
+  });
+  
+  $("#hide2").click(function() {
+$("#panel2").hide()
+  });
+  
+  $("#show3").click(function() {
+$("#panel3").show()
+  });
+  
+  $("#hide3").click(function() {
+$("#panel3").hide()
+  });
+  
+  $("#show4").click(function() {
+$("#panel4").show()
+  });
+  
+   $("#hide4").click(function() {
+$("#panel4").hide()
+  });
+  
+  $("#show5").click(function() {
+$("#panel5").show()
+  });
+  
+  $("#hide5").click(function() {
+$("#panel5").hide()
+  });
+  
+  $("#show6").click(function() {
+$("#panel6").show()
+  });
+  
+  $("#hide6").click(function() {
+$("#panel6").hide()
+  });
+  
+  $("#show7").click(function() {
+$("#panel7").show()
+  });
+  
+   $("#hide7").click(function() {
+$("#panel7").hide()
+  });
+  
+   $("#show8").click(function() {
+$("#panel8").hide()
+  });
+  
+  $("#hide8").click(function() {
+$("#panel8").hide()
+  });
+  
+  $("#show9").click(function() {
+$("#panel9").show()
+  });
+  
+  $("#hide9").click(function() {
+$("#panel9").hide()
+  });
+  
+  $("#show10").click(function() {
+$("#panel10").show()
+  });
+  
+  $("#hide10").click(function() {
+$("#panel10").hide()
+  });
+  
+  });
